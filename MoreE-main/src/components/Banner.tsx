dd88433c1c9360db1c7ace833803877800dd32f5
Qwerty <houldcould78@mail.ru>
2025-01-05 23:28:07 +0300

    text

--- MoreE-main/src/components/Banner.tsx
+++ MoreE-main/src/components/Banner.tsx
@@ -25,7 +25,7 @@ export default function ImageHoverEffect() {
       {/* Hero Section */}
       <div className="flex flex-col lg:flex-row w-full mt-10 lg:mt-32 h-auto lg:h-[600px] bg-white">
         {/* Left Block: Slider */}
-        <div className="w-full h-[500px] lg:h-[510px] relative overflow-hidden">
+        <div className="w-full h-[300px] lg:h-[510px] relative overflow-hidden">
   {slides.map((slide, index) => (
     <motion.img
       key={index}
