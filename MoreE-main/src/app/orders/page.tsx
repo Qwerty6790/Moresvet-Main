d4615e84d01c9ebf91258f02485847cab6ecd210
Qwerty <houldcould78@mail.ru>
2025-02-25 10:21:50 +0300

    text

--- MoreE-main/src/app/orders/page.tsx
+++ MoreE-main/src/app/orders/page.tsx
@@ -135,7 +135,7 @@ const Orders: React.FC = () => {
           {isModalOpen && (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
               <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h2 className="text-lg font-semibold mb-4 text-gray-800">Подтверждение отмены заказа</h2>
+                <h2 className="text-lg font-semibold mb-4 text-gray-800">Подтверждение отмены</h2>
                 <p className="text-gray-700">Вы уверены, что хотите отменить этот заказ?</p>
                 <div className="flex justify-end mt-4">
                   <button className="px-4 py-2 mr-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300" onClick={handleCloseModal}>
