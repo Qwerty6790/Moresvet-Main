9160783d2b1ea3f908fa1fc3e777013a38dd3199
Qwerty <houldcould78@mail.ru>
2025-05-11 21:09:14 +0300

    text

--- MoreE-main/src/components/Catalogofsearch.tsx
+++ MoreE-main/src/components/Catalogofsearch.tsx
@@ -342,7 +342,7 @@ const CatalogOfProductSearch: React.FC<CatalogOfProductProps> = ({
                   
                   <button 
                     onClick={handleAddToCart} 
-                    className={`ml-2 relative flex items-center justify-center w-9 h-9 rounded-none ${isPurchasable ? 'bg-black text-white' : 'bg-gray-200 text-gray-400 cursor-not-allowed'}`}
+                    className={`ml-2 relative flex items-center justify-center w-9 h-9 rounded-none ${isPurchasable ? ' text-white' : 'bg-gray-200 text-gray-400 cursor-not-allowed'}`}
                     disabled={!isPurchasable}
                     title="Добавить в корзину"
                     aria-label="Добавить в корзину"
