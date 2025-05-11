248ae58fa44af925545fb14b9cd1fc2a08558e94
Qwerty <houldcould78@mail.ru>
2025-05-11 16:03:27 +0300

    text

--- MoreE-main/src/components/BrandSlider.tsx
+++ MoreE-main/src/components/BrandSlider.tsx
@@ -51,7 +51,7 @@ export default function BrandSlider() {
         if (newPos > 0) newPos = -contentWidth;
       } else {
         // Обычная скорость (2px за кадр)
-        newPos = prev - 1;
+        newPos = prev - 0.5;
         if (newPos < -contentWidth) newPos = 0;
       }
       return newPos;
