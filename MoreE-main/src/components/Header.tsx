dfe974b8554058b89eb95fc48fb1915a8e7d84d8
Qwerty <houldcould78@mail.ru>
2025-06-17 12:29:42 +0300

    text

--- MoreE-main/src/components/Header.tsx
+++ MoreE-main/src/components/Header.tsx
@@ -389,7 +389,7 @@ const Header = () => {
                         href="/products"
                         className={`text-white text-base font-medium transition duration-500 flex items-center px-4 py-2 rounded-lg ${
                           isCatalogMenuOpen 
-                            ? 'bg-transparent backdrop-blur-xl transform translate-y-1' 
+                            ? 'bg-transparent backdrop-blur-xl transition duration-500 transform translate-y-1' 
                             : 'hover:text-gray-300'
                         }`}
                       >
