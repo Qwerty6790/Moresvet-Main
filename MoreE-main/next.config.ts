914d7bfe84882087b36fa6af6b0a54c8f9ad2ef1
Qwerty <houldcould78@mail.ru>
2025-07-02 18:01:51 +0300

    text

--- MoreE-main/next.config.ts
+++ MoreE-main/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   serverExternalPackages: [],
+  analytics: {
+    vercelAnalytics: true,
+  },
 };
 
 export default nextConfig;
