36b4cab5da4a6d9bd0b1196a8b0d0a882eddc87f
Qwerty <houldcould78@mail.ru>
2025-07-02 18:27:28 +0300

    text

---
+++ MoreE-main/src/app/catalog/werkel/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WerkelPage = () => {
+  return (
+    <div>
+      <h1>Werkel Catalog</h1>
+      <p>This is the Werkel Catalog page.</p>
+    </div>
+  );
+};
+
+export default WerkelPage; 
