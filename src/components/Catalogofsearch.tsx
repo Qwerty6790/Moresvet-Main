9813b6e1f0d1d7b546079c3cd9b62527da44dcdc
Qwerty <houldcould78@mail.ru>
2025-09-06 13:01:22 +0300

    text

--- src/components/Catalogofsearch.tsx
+++ src/components/Catalogofsearch.tsx
@@ -1,5 +1,3 @@
-export { default } from './Catalogofsearch';
-
 import React, { useState, useMemo, useEffect, useRef, lazy, Suspense, useCallback } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
